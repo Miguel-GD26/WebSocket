e3e5eba37533207a22e452d3cfd039eaae666e79
Miguel-GD26 <henrygarcia2616@gmail.com>
2025-07-20 03:44:35 -0500

    v2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/websocket"
 )
@@ -55,7 +56,14 @@ func main() {
 	})
 
 	log.Println("Servidor de Chat iniciado en http://localhost:8080")
-	err := http.ListenAndServe(":8080", nil)
+	// err := http.ListenAndServe(":8080", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Println("Servidor iniciado en http://localhost:" + port)
+	err := http.ListenAndServe(":"+port, nil)
+
 	if err != nil {
 		log.Fatalf("Error fatal al iniciar servidor: %v", err)
 	}
